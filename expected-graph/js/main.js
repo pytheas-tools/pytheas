a2b749fa34f2199d313a3736b45470a14f4e2557
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-09-14 07:04:04 +0200

    JSPlumb update

--- expected-graph/js/main.js
+++ expected-graph/js/main.js
@@ -1,34 +1,57 @@
 jsPlumb.ready(function() {
     console.log('jsPlumb ready');
 
-    var j = window.j = jsPlumb.getInstance({
+    var j = (window.j = jsPlumb.getInstance({
         Container: 'canvas',
-        Connector: "Flowchart",
-        Endpoint: ["Dot", {
-            radius: 3
-        }],
-        Anchor: "Center"
-    });
+        Connector: 'Flowchart',
+        ConnectionOverlays: [
+            [
+                'Arrow',
+                {
+                    location: 1,
+                    width: 11,
+                    length: 11,
+                    id: 'ARROW'
+                }
+            ]
+        ],
+        Anchor: 'Center'
+    }));
 
-    var connectorPaintStyle = {
-        strokeWidth: 2,
-        stroke: "#61B7CF",
-        joinstyle: "round",
-        outlineStroke: "white",
-        outlineWidth: 2
-    }
+    var connectorPaintStyleBlue = {
+            strokeWidth: 2,
+            stroke: '#61B7CF',
+            joinstyle: 'round',
+            outlineStroke: 'transparent'
+        },
+        connectorPaintStyleYellow = {
+            strokeWidth: 2,
+            stroke: '#f9bb43',
+            joinstyle: 'round',
+            outlineStroke: 'transparent'
+        };
 
     j.connect({
         source: c1_1,
         target: c2_1,
-        anchors: ["Right", "Left"],
-        endpoint: "Dot"
+        anchors: ['Right', 'Left'],
+        endpoint: 'Blank',
+        paintStyle: connectorPaintStyleYellow
     });
 
     j.connect({
         source: c1_1,
         target: c1_2,
-        anchors: ["Right", "Right"],
-        endpoint: "Dot"
+        anchors: ['Right', 'Right'],
+        endpoint: 'Blank',
+        paintStyle: connectorPaintStyleYellow
+    });
+
+    j.connect({
+        source: c1_1,
+        target: c1_3,
+        anchors: ['Right', 'Right'],
+        endpoint: 'Blank',
+        paintStyle: connectorPaintStyleBlue
     });
 });

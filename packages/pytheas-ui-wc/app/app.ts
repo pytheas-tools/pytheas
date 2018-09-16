475590457e0b4c7e223f01271380da7292de3a08
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-09-16 22:19:16 +0200

    feat(UI): pure TS version / boilerplate WIP

---
+++ packages/pytheas-ui-wc/app/app.ts
@@ -0,0 +1,3 @@
+var t = 8;
+
+console.log(t);
---
+++ packages/pytheas-ui-wc/rollup-app_prod.config.js
@@ -0,0 +1,22 @@
+import rollupTypescript from 'rollup-plugin-typescript';
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+    entry: './app/app.ts',
+    dest: './dist/scripts/app.js',
+    format: 'cjs',
+    plugins: [
+        rollupTypescript({
+            typescript: require('typescript')
+        }),
+        nodeResolve({
+            jsnext: true,
+            main: true
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            presets: 'es2015-rollup'
+        })
+    ]
+};

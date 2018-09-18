17d21d180f3979bd9d68e402e8c1dde6452a4e94
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-09-18 04:36:01 +0200

    feat(UI): use class for split.js

--- packages/pytheas-ui-wc/app/app.ts
+++ packages/pytheas-ui-wc/app/app.ts
@@ -2,6 +2,6 @@ import { defineCustomElements } from '../components/codeblock';
 defineCustomElements(window);
 
 import Split from 'split.js';
-Split(['#three', '#four'], {
+Split(['.graph-window', '.code-window'], {
     sizes: [50, 50]
 });

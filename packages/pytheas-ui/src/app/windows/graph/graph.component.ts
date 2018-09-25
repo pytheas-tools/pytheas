5acb9c5ce08df12de6fae554552db9a064ba9fff
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-09-25 15:39:01 +0200

    Cleaning

--- packages/pytheas-ui/src/app/windows/graph/graph.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-graph',
-    templateUrl: './graph.component.html',
-    styleUrls: ['./graph.component.scss']
-})
-export class GraphComponent {}

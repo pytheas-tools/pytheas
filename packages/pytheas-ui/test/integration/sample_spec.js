18d0cc5999ff07107c996fd0f6c97923cc5fd16b
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-09-27 22:27:50 +0200

    feat(test): log cypress console

--- packages/pytheas-ui/test/integration/sample_spec.js
+++ packages/pytheas-ui/test/integration/sample_spec.js
@@ -1,3 +1,9 @@
+beforeEach(() => {
+    cy.on('window:before:load', win => {
+        cy.spy(win.console, 'log');
+    });
+});
+
 describe('My First Test', function() {
     it('Does not do much!', function() {
         //cy.fixture('game.js').as('file');

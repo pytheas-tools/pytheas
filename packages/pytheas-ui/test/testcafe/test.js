f94b81859b0c9efaf790d1b7e72f474b0e5a47e9
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-09-30 10:15:09 +0200

    feat(test): browserstack

--- packages/pytheas-ui/test/testcafe/test.js
+++ packages/pytheas-ui/test/testcafe/test.js
@@ -7,10 +7,10 @@ const startMockDrag = ClientFunction(() => {
 fixture`Home page`.page`http://localhost:8383`;
 
 test('Code Window Test - Should display a code block', async t => {
-    await t.wait(5000); // Await async resources loaded...
+    await t.wait(10000); // Await async resources loaded...
     await startMockDrag();
     await t
-        .wait(500)
+        .wait(1000)
         .expect(Selector('.code-window').find('py-codeblock').exists)
         //.expect(Selector(() => document.querySelector('#host').shadowRoot.querySelector('.filename')).contains('game.js'))
         .ok();

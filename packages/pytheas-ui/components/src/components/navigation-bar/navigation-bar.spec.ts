6a550eaa1f2e470f9ed9282976105ef78eb8f0ea
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-10-06 00:35:12 +0200

    feat(UI): NavigationBar first test

---
+++ packages/pytheas-ui/components/src/components/navigation-bar/navigation-bar.spec.ts
@@ -0,0 +1,8 @@
+// import { TestWindow } from '@stencil/core/testing';
+import { NavigationBar } from './navigation-bar';
+
+describe('NavigationBar', () => {
+    it('should build', () => {
+        expect(new NavigationBar()).toBeTruthy();
+    });
+});

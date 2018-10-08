9ed30a5b09be3b2d4ccdafd193494b8e6c2f0f1f
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-10-08 22:42:43 +0200

    feat(UI): codeblock wc

---
+++ packages/pytheas-ui/components/src/components/codeblock/codeblock.spec.ts
@@ -0,0 +1,33 @@
+// import { TestWindow } from '@stencil/core/testing';
+import { CodeBlock } from './codeblock';
+
+describe('codeblock', () => {
+    it('should build', () => {
+        expect(new CodeBlock()).toBeTruthy();
+    });
+
+    /*describe('rendering', () => {
+        let element: HTMLCodeBlockElement;
+        let testWindow: TestWindow;
+        beforeEach(async () => {
+            testWindow = new TestWindow();
+            console.log(testWindow);
+
+            element = await testWindow.load({
+                components: [CodeBlock],
+                html: '<py-codeblock></py-codeblock>'
+            });
+            console.log(element);
+        });
+
+        it('should work with a filename attribute', async () => {
+            element.filename = 'player.ts';
+
+            await testWindow.flush();
+
+            console.log(element.textContent);
+
+            expect(element.textContent.trim()).toEqual(`player.ts`);
+        });
+    });*/
+});

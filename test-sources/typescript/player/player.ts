61f601fc36792e5f9d8669e8d3f9b3f28b8b6b66
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-03-15 16:13:22 +0100

    feat(test-sources): updates

--- test-sources/typescript/player/player.ts
+++ test-sources/typescript/player/player.ts
@@ -1,13 +1,17 @@
 import { PLAYER_TYPES } from './player-types';
+import { Weapon } from '../weapon/weapon';
 
 export class Player {
     name: string;
     power: number;
 
+    weapon: Weapon;
+
     type = PLAYER_TYPES.SOLDIER;
 
     constructor(name: string) {
         this.name = name;
+        this.weapon = new Weapon(`weapon-${name}`);
     }
 
     init(power: number) {

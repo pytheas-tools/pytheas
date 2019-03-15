5dee9dd181b57f419e44914390124dcdd053e9bd
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-03-15 14:31:50 +0100

    feat(UI): TS - private/public

--- packages/pytheas-ui/app/background/data/ecmascript/ecmascript-method.ts
+++ packages/pytheas-ui/app/background/data/ecmascript/ecmascript-method.ts
@@ -1,6 +1,8 @@
 import { PyElement } from '../py-element';
 import tsAstFinder, { NodeObject, TokenObject } from './ts-ast-finder';
 
+import ECMAScriptParser from './ecmascript-parser';
+
 export class ECMAScriptMethod extends PyElement {
     kind = 'method';
     constructor(methodNode: NodeObject) {
@@ -8,7 +10,7 @@ export class ECMAScriptMethod extends PyElement {
         this.name = tsAstFinder.getName(methodNode);
         if (methodNode.modifiers && methodNode.modifiers.length > 0) {
             this.modifiers = methodNode.modifiers.map((modifier: TokenObject) => {
-                return modifier.text;
+                return ECMAScriptParser.getSyntaxKindName(modifier.kind);
             });
         }
     }
